a05b46626edfe1c567682ea6e5966c08dd41e8a4
nachatjatu <160994837+nachatjatu@users.noreply.github.com>
2026-02-11 05:16:20 +0000

    Deploying to gh-pages from @ nachatjatu/nachatjatu.github.io@ab7c75e947c9a38a8088d45f96a119a2bab0ee98 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-education",
           title: "education",
-          description: "education",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/education/";
