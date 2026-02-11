3d47c87d4ca3590f4a9977bc8fc86eaf09d36af7
nachatjatu <160994837+nachatjatu@users.noreply.github.com>
2026-02-11 04:50:49 +0000

    Deploying to gh-pages from @ nachatjatu/nachatjatu.github.io@8a7aaea8e5046bfb307da298e7fea190fd156e51 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
-        },{id: "nav-people",
+  },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
           section: "Navigation",
