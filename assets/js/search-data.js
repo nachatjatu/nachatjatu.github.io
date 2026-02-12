9d993a8bd2f4657b878c4033225c5ba034cef05a
nachatjatu <160994837+nachatjatu@users.noreply.github.com>
2026-02-12 08:31:40 +0000

    Deploying to gh-pages from @ nachatjatu/nachatjatu.github.io@55af9cb0a330c6175f7936d6f0b1c454bf0b9877 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,11 +17,11 @@ ninja.data = [{
             window.location.href = "/education/";
           },
         },{id: "nav-cv",
-          title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "cv",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/_pages/cv/";
           },
         },{id: "post-a-post-with-plotly-js",
         
